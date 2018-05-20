b93952da308f6edd2605259526a4e7003a5899d7
Rafał Petryka <rafalpetryka@gmail.com>
2018-05-20 11:41:28 +0200

    Added api.js file

---
+++ src/api.js
@@ -0,0 +1,19 @@
+const API_URL = 'http://warsawjs-flights-api.herokuapp.com/';
+
+let readAirportList = () => {
+    return window.fetch(API_URL + 'airports').then((response) => {
+        response.json();
+    }).catch((error) => {
+        console.error(error);
+    })
+}
+
+let searchFlights = (params) => {
+    const { from, to, departDate, returnDate } = params
+    return window.fetch(`${API_URL}flights/${from}/${to}/${departDate}/${returnDate}`)
+        .then((response) => {
+            response.json();
+        }).catch((error) => {
+            console.error(error);
+        })
+}
